d77fce4ee27d277a2bd6ae6929a03e4d744140f5
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-14 20:22:56 +0900

    LazyVGrid

--- SampleLazyVGrid/ContentView.swift
+++ SampleLazyVGrid/ContentView.swift
@@ -9,13 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        let symbols = [
+            "pencil",
+            "pencil.circle",
+            "pencil.circle.fill",
+            "pencil.slash",
+            "square.and.pencil",
+            "rectangle.and.pencil.and.ellipsis",
+            "highlighter",
+            "pencil.and.outline",
+            "pencil.tip",
+            "pencil.tip.crop.circle",
+            "pencil.tip.crop.circle.badge.plus",
+            "pencil.tip.crop.circle.badge.minus",
+            "pencil.tip.crop.circle.badge.arrow.forward",
+            "lasso",
+            "lasso.and.sparkles",
+            "trash",
+            "trash.fill",
+            "trash.circle",
+            "trash.circle.fill",
+            "trash.square",
+            "trash.square.fill",
+            "trash.slash"
+        ]
+        // 列の数を決める
+        let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 4)
+        
+        ScrollView {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(symbols, id: \.self) { symbol in
+                    Image(systemName: symbol)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .padding(20)
+                        .background(.black)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
