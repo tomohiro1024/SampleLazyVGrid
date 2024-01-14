39013109f36629483af3be7e95398e38d3d39cc3
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-14 15:13:02 +0900

    first commit

---
+++ SampleLazyVGrid/SampleLazyVGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleLazyVGridApp.swift
+//  SampleLazyVGrid
+//
+//  Created by 宮野智宏 on 2024/01/14.
+//
+
+import SwiftUI
+
+@main
+struct SampleLazyVGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
