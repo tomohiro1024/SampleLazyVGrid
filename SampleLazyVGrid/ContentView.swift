b01c27a19e29f0c1f0f10b019a8285e0a600c696
tomohiro1024 <gongyezhihong732@gmail.com>
2024-01-15 16:44:17 +0900

    @stateについて

--- SampleLazyVGrid/ContentView.swift
+++ SampleLazyVGrid/ContentView.swift
@@ -8,49 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOn = false
+    @State var inputText = ""
+    
     var body: some View {
-        let symbols = [
-            "pencil",
-            "pencil.circle",
-            "pencil.circle.fill",
-            "pencil.slash",
-            "square.and.pencil",
-            "rectangle.and.pencil.and.ellipsis",
-            "highlighter",
-            "pencil.and.outline",
-            "pencil.tip",
-            "pencil.tip.crop.circle",
-            "pencil.tip.crop.circle.badge.plus",
-            "pencil.tip.crop.circle.badge.minus",
-            "pencil.tip.crop.circle.badge.arrow.forward",
-            "lasso",
-            "lasso.and.sparkles",
-            "trash",
-            "trash.fill",
-            "trash.circle",
-            "trash.circle.fill",
-            "trash.square",
-            "trash.square.fill",
-            "trash.slash"
-        ]
-        // 列の数を決める
-        let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 4)
-        
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                ForEach(symbols, id: \.self) { symbol in
-                    Image(systemName: symbol)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding(20)
-                        .background(.black)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                    
+        VStack {
+            Toggle(isOn: $isOn) {
+                if isOn {
+                    Text("ON")
+                } else {
+                    Text("OFF")
                 }
             }
+            .padding()
+            TextField("ここにヒント", text: $inputText)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            Text("\(inputText)さん、こんにちは！")
         }
+        
+        
     }
 }
 
